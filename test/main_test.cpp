f9b74e26647566d3550caac00ef6f5e4e963d148
Francisco Aguilera <Falven2000@Hotmail.com>
2016-05-03 20:50:34 -0600

    Added CTest.

---
+++ test/main_test.cpp
@@ -0,0 +1,14 @@
+//
+// Created by Francisco Aguilera on 5/3/16.
+//
+
+#include <iostream>
+#include <boost/asio.hpp>
+
+using namespace std;
+
+int main(int argc, const char * argv [])
+{
+	cout << "Running Test!" << endl;
+	return 0;
+}
